872263216a1b5315a090f46a6c5d51214492d6cf
Quentin <xquentin_gille@hotmail.com>
2018-12-12 13:52:52 +0100

    improved promises

--- functions.js
+++ functions.js
@@ -1,3 +1,10 @@
+var Gpio = require('pigpio').Gpio;
+
+var ledRed = new Gpio(27, {mode: Gpio.OUTPUT});
+var ledGreen = new Gpio(17, {mode: Gpio.OUTPUT});
+var ledBlue = new Gpio(22, {mode: Gpio.OUTPUT});
+
+
 class Lights {
   constructor(delay) {
     this.delay = delay;
@@ -6,29 +13,29 @@ class Lights {
   setColor(color, dutyCycle) {
     switch (color) {
       case "RED":
-        this.ledRed.pwmWrite(dutyCycle);
+        //ledRed.pwmWrite(dutyCycle);
         break;
       case "BLUE":
-        this.ledBlue.pwmWrite(dutyCycle);
+        //ledBlue.pwmWrite(dutyCycle);
         break;
       case "GREEN":
-        this.ledGreen.pwmWrite(dutyCycle);
+        //ledGreen.pwmWrite(dutyCycle);
         break;
       default:
         break;
     }
   }
 
-  fadeIn(color, from, to) {
+  fadeIn(color, dutyCycle) {
     return new Promise((resolve, reject) => {
       if (!this[`fadeIn${color}`]) {
         this[`fadeIn${color}`] = resolve
       }
       setTimeout(() => {
-        //console.log(`[FadeIn] ${color} set to ${from}`);
-        this.setColor(color, from);
-        from += 5;
-        if (from <= to) this.fadeIn(color, from);
+        console.log(`[FadeIn] ${color} set to ${dutyCycle}`);
+        this.setColor(color, dutyCycle);
+        dutyCycle += 5;
+        if (dutyCycle <= 255) this.fadeIn(color, dutyCycle);
         else {
         	this[`fadeIn${color}`](this);
           this[`fadeIn${color}`] = null;
@@ -37,7 +44,7 @@ class Lights {
     });
   }
 
-  fadeOut(color, from, to) {
+  fadeOut(color, dutyCycle) {
     return new Promise((resolve, reject) => {
       if (!this[`fadeOut${color}`]) {
         this[`fadeOut${color}`] = resolve
@@ -45,10 +52,10 @@ class Lights {
 
 
       setTimeout(() => {
-        //console.log(`[FadeOut] ${color} set to ${from}`);
-        this.setColor(color, from);
-        from -= 5;
-        if (from >= to) this.fadeOut(color, from);
+        console.log(`[FadeOut] ${color} set to ${dutyCycle}`);
+        this.setColor(color, dutyCycle);
+        dutyCycle -= 5;
+        if (dutyCycle >= 0) this.fadeOut(color, dutyCycle);
         else {
         	this[`fadeOut${color}`](this);
           this[`fadeOut${color}`] = null;
@@ -63,10 +70,10 @@ var lights = new Lights(50);
 
 (async () => {
 	await Promise.all([
-  	lights.fadeIn('BLUE', 0, 255),
-    lights.fadeIn('RED', 100, 255)
+  	lights.fadeIn('BLUE', 0),
+    lights.fadeIn('RED', 100)
    ])
-   await lights.fadeOut('RED', 150, 0);
-   console.log('Done')
+   await lights.fadeOut('RED', 150);
+   console.log('DOne')
 })()
 
