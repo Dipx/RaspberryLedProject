8d02a8b9735865f185eb91c7e259c45a88064ff6
quentin <xquentin_gille@hotmail.com>
2018-12-12 11:37:24 +0100

    continue

--- functions.js
+++ functions.js
@@ -1,40 +1,66 @@
+var Gpio = require('pigpio').Gpio;
+
+
+
 class Lights {
+
     constructor(delay) {
-      this.blue = 0;
-      this.green = 0;
-      this.red = 0;
+      this.ledRed = new Gpio(27, {mode: Gpio.OUTPUT});
+      this.ledGreen = new Gpio(17, {mode: Gpio.OUTPUT});
+      this.ledBlue = new Gpio(22, {mode: Gpio.OUTPUT});
+
       this.delay = delay;
       this.fadeInResolve = null;
       this.fadeOutResolve = null;
     }
-  
-    fadeIn(color, i) {
+    
+    setColor(color, dutyCycle) {
+      switch (color) {
+        case "RED":
+          this.ledRed.pwmWrite(dutyCycle);
+          break;
+        case "BLUE":
+          this.ledBlue.pwmWrite(dutyCycle);
+          break;
+        case "GREEN":
+          this.ledGreen.pwmWrite(dutyCycle);
+          break;
+        default:
+          break;
+      }
+    }
+
+    fadeIn(color, dutyCycle) {
       return new Promise((resolve, reject) => {
         if (!this.fadeInResolve) {
           this.fadeInResolve = resolve
         }
-  
+        console.log(color + " fadein begin");
         setTimeout(() => {
-          this[color] = i;
-          console.log(this[color]);
-          i += 5;
-          if (i <= 255) this.fadeIn(color, i);
+
+          this.setColor(color, dutyCycle);
+          
+          console.log(dutyCycle);
+          dutyCycle += 5;
+          if (dutyCycle <= 255) this.fadeIn(color, dutyCycle);
           else this.fadeInResolve(this)
         }, this.delay);
       });
     }
   
-    fadeOut(color, i) {
+    fadeOut(color, dutyCycle) {
       return new Promise((resolve, reject) => {
         if (!this.fadeOutResolve) {
           this.fadeOutResolve = resolve
         }
+
+        console.log(color + " fadeout begin");
   
         setTimeout(() => {
-          this[color] = i;
-          console.log(this[color]);
-          i -= 5;
-          if (i >= 0) this.fadeOut(color, i);
+          this.setColor(color, dutyCycle);
+          console.log(dutyCycle);
+          dutyCycle -= 5;
+          if (dutyCycle >= 0) this.fadeOut(color, dutyCycle);
           else this.fadeOutResolve(this)
         }, this.delay);
       });
@@ -44,9 +70,25 @@ class Lights {
   
   var lights = new Lights(50);
   
-  lights.fadeIn("blue", 0).then(() => {
-    console.log('Fade in done')
-    lights.fadeOut("blue", 255).then(() => {
-      console.log('Fade out done')
+  // lights.fadeIn("blue", 0).then(() => {
+  //   console.log('Fade in done')
+  //   lights.fadeOut("blue", 255).then(() => {
+  //     console.log('Fade out done')
+  //   })
+  // });
+
+  // lights.fadeIn("BLUE", 0).then(() => {
+  //   lights.fadeOut("BLUE", 255).then(() => {
+  //     lights.fadeIn("RED", 0).then(() => {
+  //       lights.fadeOut("RED", 255);
+  //     })
+  //   })
+  // });
+
+  lights.fadeIn("BLUE", 0).then(() => {
+    lights.fadeOut("BLUE, 255");
+  }).then(() => {
+    lights.fadeIn("RED", 0).then(() => {
+      lights.fadeOut("RED", 255);
     })
-  });
+  })
